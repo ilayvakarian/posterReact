216cb2e7f62217a61e8d607dbe9a0c50ec8f29aa
ilayvakarian <ghost16997@gmail.com>
2020-03-30 20:21:12 +0300

    Updates

---
+++ precache-manifest.dba5ec14d94d1048de2e3fbe8a6032c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23de5315d7f137a19bef107542e99a4e",
+    "url": "/posterReact/index.html"
+  },
+  {
+    "revision": "0ec2761f7a549259eca9",
+    "url": "/posterReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0ec2761f7a549259eca9",
+    "url": "/posterReact/static/js/2.49eed82f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/posterReact/static/js/2.49eed82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012b3a7cade4ea6f4453",
+    "url": "/posterReact/static/js/main.4d171ed1.chunk.js"
+  },
+  {
+    "revision": "310f0b8544d61e189d3f",
+    "url": "/posterReact/static/js/runtime-main.f3525944.js"
+  }
+]);
